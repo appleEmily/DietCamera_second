028b6a5db2f7b1a8dad19adf87aa03687c6b4ee9
Emily Nozaki <emiri3128@gmail.com>
2020-05-08 00:15:40 +0900

    Initial Commit

---
+++ DietCamera_second/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DietCamera_second
+//
+//  Created by Emily Nozaki on 2020/05/08.
+//  Copyright © 2020 Emily Nozaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
